6987a9a37c1b10227a1a803e8b96037de178c659
EmeryP <emeryparks09@hotmail.com>
2018-10-22 15:22:07 -0700

    fixed tests

--- code-challenges-05/linked-list.js
+++ code-challenges-05/linked-list.js
@@ -1,5 +1,4 @@
 
-
 class Node {                                                                                        
   constructor(data, next = null){
     this.data = data;
@@ -8,7 +7,6 @@ class Node {
 }
 
 class LinkedList {
-
   constructor(node = null){ 
     this.head = node;
   }
@@ -17,7 +15,7 @@ class LinkedList {
   isEmpty(){
     return this.head === null;
   }
-
+  
   //////////////////////////////
   size(){
     var current = this.head;
@@ -29,68 +27,54 @@ class LinkedList {
     }
     return count;
   }
-
+  
   //////////////////////////////
   insert(val){ 
-
     const oldHead = this.head
     const newNode = new Node(val, oldHead)
-    this.head = newNode
+    this.head = newNode    
   }
   
   /////////////////////////////
   includes(val){
     var current = this.head;
-      while(current !== null){
+    while(current !== null){
         if(current.data === val){
           return true;
         }
         current = current.next;
       }
       return false;
-  }
-
-  /////////////////////////////
-  append(val){
-    let current = this.head;
-
-    while(current.next !== null){
-      current = current.next
     }
+    
+    /////////////////////////////
+    append(val){
+      let current = this.head;
+      
+      while(current.next !== null){
+        current = current.next
+      }
     current.next = new Node(val)
-    console.log(current.next)
   }
-
+  
   /////////////////////////////
   print(){
     var output = '[';
     var current = this.head;
 
     while(current != null){
-        output += current.data;
+      output += current.data;
         if(current.next !== null){
-            output += ',';
-          }
-          current = current.next;
+          output += ',';
+        }
+        current = current.next;
+      }
+      output += ']';
+      console.log(output);
+      return output;
     }
-    output += ']';
-    console.log(output);
-    return output;
   }
 
-}
+module.exports = LinkedList
 
-var sll = new LinkedList();
-sll.insert(20)
-sll.insert(30)
-sll.insert(40)
-sll.append(50)
-sll.insert(70)
-sll.append(100)
-
-sll.print()
-
-module.exports = {
-  LinkedList, Node
-};
 
