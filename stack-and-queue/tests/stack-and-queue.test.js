e43f94c236f3c22b8ddcf5338524320ff95e954f
EmeryP <emeryparks09@hotmail.com>
2018-10-26 18:51:07 -0700

    pop tests functioning

--- stack-and-queue/tests/stack-and-queue.test.js
+++ stack-and-queue/tests/stack-and-queue.test.js
@@ -6,7 +6,7 @@ describe('Stacks', () => {
 
   describe('Stacks -> Push', () => {
 
-    it('The stack to be defined', () => {
+    it('The stack to be defined after a single push', () => {
       const sandq = new stacksAndQueues();
       sandq.push(10);
       expect(sandq).toBeDefined();
@@ -30,7 +30,35 @@ describe('Stacks', () => {
 
   });
 
-  xdescribe('Peek', () => {
+  describe('Stacks -> Pop', () => {
+
+    xit('The pop method to be defined', () => {
+      const sandq = new stacksAndQueues();
+      sandq.pop();
+      expect(sandq).toBeDefined();
+    });
+
+    it('The length of the storage array should be (3 - 1) = 2', () => {
+      const sandq = new stacksAndQueues();
+      sandq.push(10);
+      sandq.push(30);
+      sandq.push(40);
+      sandq.pop();
+      expect(sandq.storage.length).toBe(2);
+    });
+
+    it('The popped off value should be equal to 40', () => {
+      const sandq = new stacksAndQueues();
+      sandq.push(10);
+      sandq.push(30);
+      sandq.push(40);
+      expect(sandq.pop().value).toBe(40);
+      // console.log(sandq.pop().value);
+    });
+
+  });
+
+  xdescribe('Stacks -> Peek', () => {
 
     it('should test that peek is defined', () => {
       const sandq = new stacksAndQueues();
